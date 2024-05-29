f5cd1ff457da530fb8a2c32a11c7d8ec7338fb43
yijie4188 <yijie4188@gmail.com>
2024-05-30 03:48:38 +0800

    feat: import shikitor plugins

--- renderer/components/Sender.tsx
+++ renderer/components/Sender.tsx
@@ -1,5 +1,12 @@
 import './Sender.scss'
+import '@shikitor/core/plugins/provide-completions.css'
+import '@shikitor/core/plugins/provide-popup.css'
+import '@shikitor/core/plugins/provide-selection-toolbox.css'
 
+import provideCompletions from '@shikitor/core/plugins/provide-completions'
+import providePopup from '@shikitor/core/plugins/provide-popup'
+import provideSelectionToolbox from '@shikitor/core/plugins/provide-selection-toolbox'
+import selectionToolboxForMd from '@shikitor/core/plugins/selection-toolbox-for-md'
 import { Editor } from '@shikitor/react'
 import { useState } from 'react'
 
@@ -9,6 +16,13 @@ import { useColor } from '../hooks/useColor'
 export interface SenderProps {
   onSend(text: string, dispatch: (text: string) => void): void
 }
+
+const plugins = [
+  providePopup,
+  provideCompletions,
+  provideSelectionToolbox,
+  selectionToolboxForMd
+]
 
 export function Sender(props: SenderProps) {
   const prefix = 'spirit-sender'
@@ -29,6 +43,7 @@ export function Sender(props: SenderProps) {
         lineNumbers: 'off',
         theme: 'material-theme-darker'
       }}
+      plugins={plugins}
       onColorChange={setColor}
       onMounted={shikitor => {
         shikitor.focus()
