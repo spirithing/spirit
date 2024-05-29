9a38297c56c4b542f66da2380f6f9b167dc408d1
yijie4188 <yijie4188@gmail.com>
2024-05-30 02:08:26 +0800

    style: eslint

--- renderer/main.tsx
+++ renderer/main.tsx
@@ -3,6 +3,7 @@ import 'tdesign-react/es/style/index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import { App } from './App'
 
 createRoot(document.getElementById('root') as HTMLElement)
