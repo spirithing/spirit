68a8b4163dfbad5bad804b32925f554e1e0709ba
yijie4188 <yijie4188@gmail.com>
2024-05-31 04:35:34 +0800

    feat: implement theme switch

--- renderer/main.tsx
+++ renderer/main.tsx
@@ -7,12 +7,15 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 import { I18NProvider } from './providers/i18n'
+import { ThemeProvider } from './providers/theme'
 
 createRoot(document.getElementById('root') as HTMLElement)
   .render(
     <StrictMode>
       <I18NProvider>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </I18NProvider>
     </StrictMode>
   )
---
+++ renderer/providers/theme.tsx
@@ -0,0 +1,48 @@
+import type { ReactNode } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
+
+import { useElectronStore } from '../store'
+
+type Theme = 'light' | 'dark' | (string & {})
+
+const ThemeContext = createContext<Theme>('light')
+
+export function ThemeProvider({ children }: { children: ReactNode }) {
+  const [common] = useElectronStore('common')
+  const theme = common?.theme ?? 'auto'
+  const [systemTheme, setSystemTheme] = useState<Theme>(() =>
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .matches
+      ? 'dark'
+      : 'light'
+  )
+  useEffect(() => {
+    const mediaQueryListDark = window.matchMedia('(prefers-color-scheme: dark)')
+    const listenMediaQueryListDark = (mediaQueryListEvent: MediaQueryListEvent) => {
+      setSystemTheme(mediaQueryListEvent.matches ? 'dark' : 'light')
+    }
+    mediaQueryListDark.addListener(listenMediaQueryListDark)
+    return () => mediaQueryListDark.removeListener(listenMediaQueryListDark)
+  }, [])
+  const calcTheme = useMemo(() => {
+    if (theme === 'auto') {
+      return systemTheme
+    }
+    return theme
+  }, [systemTheme, theme])
+  useEffect(() => {
+    if (calcTheme === 'dark') {
+      document.documentElement.setAttribute('theme-mode', 'dark')
+    } else {
+      document.documentElement.removeAttribute('theme-mode')
+    }
+  }, [calcTheme])
+  return (
+    <ThemeContext.Provider value={calcTheme}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export const useTheme = () => useContext(ThemeContext)
