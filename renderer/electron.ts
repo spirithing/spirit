022e2817a328a79ee93a5f47a61d3c8b66db4a36
yijie4188 <yijie4188@gmail.com>
2024-06-02 21:37:07 +0800

    feat: forbidden ipc call loop

--- renderer/electron.ts
+++ renderer/electron.ts
@@ -1,3 +1,29 @@
-export const ipcRenderer = electron.ipcRenderer
+// noinspection TypeScriptUnresolvedReference
+
+const MAX_IPC_CALLS = 50
+
+let count = 0
+const i = setInterval(() => count = 0, 100)
+if (import.meta.hot) {
+  import.meta.hot.accept()
+  import.meta.hot.dispose(() => clearInterval(i))
+}
+
+export const ipcRenderer = new Proxy(electron.ipcRenderer, {
+  get(target, prop) {
+    const field = Reflect.get(target, prop)
+    if (typeof prop === 'string' && typeof field === 'function') {
+      return (...args: unknown[]) => {
+        if (count > MAX_IPC_CALLS) {
+          throw new Error('IPC call loop detected')
+        }
+        count++
+        return field(...args)
+      }
+    } else {
+      return field
+    }
+  }
+})
 export const webFrame = electron.webFrame
 export const process = electron.process
