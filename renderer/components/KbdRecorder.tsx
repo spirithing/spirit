8814686cd5d43e791391626d70fccef915c8cbe3
yijie4188 <yijie4188@gmail.com>
2024-06-06 02:12:59 +0800

    feat: reset recorder result

--- renderer/components/KbdRecorder.tsx
+++ renderer/components/KbdRecorder.tsx
@@ -1,29 +1,57 @@
 import './KbdRecorder.scss'
 
-import { CSSProperties, useEffect } from 'react'
-import { useRef, useState } from 'react'
+import type { CSSProperties } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { Button } from 'tdesign-react'
 import useControlled from 'tdesign-react/es/hooks/useControlled'
 
+import { useEventCallback } from '../hooks/useEventCallback'
 import { classnames } from '../utils/classnames'
 import { Kbd } from './Kbd'
 
 export interface KbdRecorderProps {
   style?: CSSProperties
   className?: string
+  clearable?: boolean
+  resettable?: boolean
   value?: string[]
   defaultValue?: string[]
   onChange?: (value?: string[]) => void
+  onRecordStart?: () => void
+  onRecordEnd?: () => void
 }
 
 export function KbdRecorder(props: KbdRecorderProps) {
   const { prefix } = KbdRecorder
-  const { className, style } = props
-  const [recording, setRecording] = useState(false)
+  const {
+    className,
+    style,
+    clearable,
+    resettable,
+    onRecordStart,
+    onRecordEnd
+  } = props
   const [keys, setKeys] = useControlled(props, 'value', props.onChange!)
-  useEffect(() => {
-    if (!recording) return
-  }, [recording])
+  const recordStart = useEventCallback(onRecordStart)
+  const recordEnd = useEventCallback(onRecordEnd)
+
+  const [recording, setRecording] = useState(false)
+  const keysBeforeRecording = useRef(keys)
+  const toggleRecording = useCallback((must?: boolean) => {
+    const next = must ?? !recording
+    setRecording(next)
+    if (next) {
+      keysBeforeRecording.current = keys
+      recordStart?.()
+    } else {
+      recordEnd?.()
+    }
+    if (must === false) {
+      if (keys && ['meta', 'ctrl', 'opt', 'shift'].includes(keys[keys.length - 1])) {
+        setKeys(keysBeforeRecording.current)
+      }
+    }
+  }, [keys, recordStart, recordEnd, recording, setKeys])
 
   const recordRef = useRef<HTMLElement>(null)
   return <div
@@ -38,8 +66,8 @@ export function KbdRecorder(props: KbdRecorderProps) {
         variant='text'
         shape='circle'
         theme='danger'
-        onClick={() => setRecording(!recording)}
-        onBlur={() => setRecording(false)}
+        onClick={() => toggleRecording()}
+        onBlur={() => toggleRecording(false)}
         onKeyDown={e => {
           if (!recording) return
 
@@ -58,14 +86,23 @@ export function KbdRecorder(props: KbdRecorderProps) {
           ? <span className='s-icon'>radio_button_checked</span>
           : <span className='s-icon'>radio_button_unchecked</span>}
       </Button>
-      <Button
+      {clearable && <Button
         size='small'
         variant='text'
         shape='circle'
         onClick={() => setKeys([])}
       >
         <span className='s-icon'>cancel</span>
-      </Button>
+      </Button>}
+      {resettable && props.defaultValue && props.defaultValue.length > 0 && <Button
+        size='small'
+        variant='text'
+        shape='circle'
+        disabled={keys?.every((key, index) => key === props.defaultValue![index])}
+        onClick={() => setKeys(props.defaultValue)}
+      >
+        <span className='s-icon'>reset_wrench</span>
+      </Button>}
     </div>
   </div>
 }
