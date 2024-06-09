c4468c0972adcd4cd169b9fb7ba7f297eba594c0
yijie4188 <yijie4188@gmail.com>
2024-06-09 14:38:53 +0800

    feat: add os conditional

--- main/utils/system.ts
+++ main/utils/system.ts
@@ -8,4 +8,7 @@ export const userDataPath = app.getPath('userData')
 export const username = userinfo.username
 
 const osType = os.type()
+export const isWindows = osType === 'Windows_NT'
+export const isMac = osType === 'Darwin'
+export const isLinux = osType === 'Linux'
 export { osType as os }
