7146fe841d72085cf6d749beaa7e4018c2d27856
yijie4188 <yijie4188@gmail.com>
2024-06-10 02:32:59 +0800

    feat: support resolve metaOrCtrl by platform

--- renderer/utils/isShortcut.ts
+++ renderer/utils/isShortcut.ts
@@ -1,15 +1,19 @@
 export const withKeys = ['meta', 'metaOrCtrl', 'ctrl', 'shift', 'alt'] as const
 export type Shortcut = (typeof withKeys)[number] | (string & {})
+function mappingMetaOrCtrl<T extends Shortcut>(key: T) {
+  return key === 'metaOrCtrl'
+    ? navigator.platform.includes('Mac')
+      ? 'meta'
+      : 'ctrl'
+    : key
+}
 export function isShortcut(e: KeyboardEvent, keys: Shortcut[]) {
-  let rerefKeys = [...keys]
+  const rerefKeys = [...keys].map(mappingMetaOrCtrl)
   for (let withKey of withKeys) {
-    if (withKey === 'metaOrCtrl') {
-      withKey = navigator.platform.includes('Mac') ? 'meta' : 'ctrl'
-    }
+    withKey = mappingMetaOrCtrl(withKey)
     // when withKey in keys, check e[`${key}Key`] is true, else assert e[`${key}Key`] is false
     if (rerefKeys.includes(withKey)) {
       if (!e[`${withKey}Key`]) return false
-      rerefKeys = rerefKeys.filter(key => key !== withKey)
     } else {
       if (e[`${withKey}Key`]) return false
     }
