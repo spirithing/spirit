c432b4ef005e01d3ba1d454b5cf043c7b4d670e9
yijie4188 <yijie4188@gmail.com>
2024-06-23 01:25:17 +0800

    refactor: remove useless openai service atom

--- renderer/hooks/useOpenAI.ts
+++
@@ -1,6 +0,0 @@
-import { useAtom } from 'jotai'
-
-import { openAIAtom } from '../atoms/openAI'
-import { electronStore } from '../store'
-
-export const useOpenAI = () => useAtom(openAIAtom, { store: electronStore })[0]
