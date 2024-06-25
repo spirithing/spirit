ffad83d551b82a502630b4aa3d6a389ac0ef8245
yijie4188 <yijie4188@gmail.com>
2024-06-26 02:27:00 +0800

    feat: implement coze quester

---
+++ renderer/extensions/adapter-coze/Quester.ts
@@ -0,0 +1,223 @@
+import { parseJSON } from '../../utils/parseJSON'
+
+export interface CozeQuesterConfig {
+  apiHost: string
+  bearerToken: string
+}
+
+export interface CozeQuesterMessage {
+  role: 'assistant' | 'user'
+  type: 'answer' | 'function_call' | 'tool_response' | 'follow_up'
+  content: string
+  content_type?: 'text'
+}
+
+export interface CozeQuesterChatReq {
+  botID: string
+  conversationID?: string
+  user: string
+  query: string
+  chatHistory?: CozeQuesterMessage[]
+  stream: boolean
+  customVariables?: Record<string, string>
+}
+
+export interface CozeQuesterChatMessageSnake {
+  role: string
+  type: string
+  content: string
+  content_type: string
+  extra_info?: object | null
+}
+
+export interface CozeQuesterChatRespSnake {
+  messages: CozeQuesterChatMessageSnake[]
+  conversation_id: string
+  code: 0
+  msg: 'success'
+}
+
+export type CozeQuesterChatRespIterItemSnake =
+  | { event: 'done' }
+  | {
+    event: 'message'
+    is_finish: boolean
+    index: number
+    conversation_id: string
+    seq_id: number
+    message: CozeQuesterChatMessageSnake
+  }
+  | {
+    event: 'error'
+    error_information: {
+      code: number
+      msg: string
+    }
+  }
+
+export interface CozeQuesterChatMessage {
+  role: string
+  type: string
+  content: string
+  contentType: string
+  extraInfo?: object | null
+}
+
+export interface CozeQuesterChatResp {
+  messages: CozeQuesterChatMessage[]
+  conversationID: string
+  code: 0
+  msg: 'success'
+}
+
+export type CozeQuesterChatRespIterItem =
+  | { event: 'done' }
+  | {
+    event: 'message'
+    isFinish: boolean
+    index: number
+    conversationID: string
+    seqID: number
+    message: CozeQuesterChatMessage
+  }
+  | {
+    event: 'error'
+    errorInformation: {
+      code: number
+      msg: string
+    }
+  }
+
+export interface ErrorResponse {
+  code: number
+  msg: string
+}
+
+export default class CozeQuester {
+  private apiHost: CozeQuesterConfig['apiHost']
+  private defaultHeaders: Record<string, string> = {}
+  constructor(
+    config: CozeQuesterConfig
+  ) {
+    this.apiHost = config.apiHost
+    this.defaultHeaders = {
+      'Content-Type': 'application/json',
+      Accept: '*/*',
+      Host: 'api.coze.cn',
+      Connection: 'keep-alive',
+      Authorization: `Bearer ${config.bearerToken}`
+    }
+  }
+
+  chat(req: CozeQuesterChatReq & { stream: true }): Promise<AsyncGenerator<CozeQuesterChatRespIterItem>>
+  chat(req: CozeQuesterChatReq & { stream?: false }): Promise<CozeQuesterChatResp>
+  async chat(
+    req: CozeQuesterChatReq
+  ): Promise<CozeQuesterChatResp | AsyncGenerator<CozeQuesterChatRespIterItem>> {
+    req.stream = req.stream ?? false
+    const resp = await fetch(`${this.apiHost}/chat`, {
+      method: 'POST',
+      headers: {
+        ...this.defaultHeaders
+      },
+      body: JSON.stringify({
+        /* eslint-disable camelcase */
+        bot_id: req.botID,
+        conversation_id: req.conversationID,
+        user: req.user,
+        query: req.query,
+        chat_history: req.chatHistory,
+        stream: req.stream,
+        custom_variables: req.customVariables
+        /* eslint-enable camelcase */
+      })
+    })
+    if (!resp.body) {
+      throw new Error('Missing body')
+    }
+    const itr = parseJSON(resp.body, text => {
+      const jsonStr = text.replace(/^data:/, '')
+      return JSON.parse(jsonStr)
+    })
+    if (req.stream) {
+      const iterator = itr as AsyncGenerator<
+        CozeQuesterChatRespIterItemSnake,
+        ErrorResponse
+      >
+      return (async function*() {
+        do {
+          const message = await iterator.next()
+          if (message.done) {
+            if (message.value.code === 0) {
+              yield { event: 'done' } satisfies CozeQuesterChatRespIterItem
+            } else {
+              yield {
+                event: 'error',
+                errorInformation: message.value
+              } satisfies CozeQuesterChatRespIterItem
+            }
+            return
+          }
+          const item = message.value
+
+          if (item.event === 'done') {
+            yield { event: item.event } satisfies CozeQuesterChatRespIterItem
+            return
+          }
+          if (item.event === 'error') {
+            yield { event: item.event, errorInformation: item.error_information } satisfies CozeQuesterChatRespIterItem
+            return
+          }
+          const {
+            event,
+            message: {
+              role,
+              type,
+              content,
+              content_type: contentType,
+              extra_info: extraInfo,
+              ...restMessage
+            },
+            is_finish: isFinish,
+            index,
+            conversation_id: conversationID,
+            seq_id: seqID,
+            ...rest
+          } = item
+          yield {
+            message: { role, type, content, contentType, extraInfo, ...restMessage },
+            event,
+            isFinish,
+            index,
+            conversationID,
+            seqID,
+            ...rest
+          } satisfies CozeQuesterChatRespIterItem
+        } while (true)
+      })()
+    } else {
+      const iterator = itr as AsyncGenerator<unknown, CozeQuesterChatRespSnake | ErrorResponse>
+      const message = await iterator.next()
+      if (!message.done) {
+        throw new Error('Unexpected response')
+      }
+      if (message.value.code === 0) {
+        const value = message.value as CozeQuesterChatRespSnake
+        return {
+          messages: value.messages.map(m => ({
+            role: m.role,
+            type: m.type,
+            content: m.content,
+            contentType: m.content_type,
+            extraInfo: m.extra_info
+          })),
+          conversationID: value.conversation_id,
+          code: value.code,
+          msg: value.msg
+        }
+      } else {
+        throw new Error(`[${message.value.code}] ${message.value.msg}`)
+      }
+    }
+  }
+}
---
+++ renderer/extensions/adapter-coze/tests/Quester.spec.ts
@@ -0,0 +1,86 @@
+import { describe, expect, expectTypeOf, test, vi } from 'vitest'
+
+import type { CozeQuesterChatResp, CozeQuesterChatRespIterItem } from '../Quester'
+import CozeQuester from '../Quester'
+
+const botID = process.env.COZE_BOT_ID as string
+
+describe('Quester', () => {
+  const q = new CozeQuester({
+    apiHost: 'https://api.coze.cn/open_api/v2',
+    bearerToken: process.env.COZE_PAT as string
+  })
+  describe('no stream', () => {
+    test('base', async () => {
+      const resp = await q.chat({
+        botID,
+        user: 'yiijie',
+        query: '你好',
+        stream: false
+      })
+      expectTypeOf(resp).toEqualTypeOf<CozeQuesterChatResp>()
+      expect(resp.code).eq(0)
+      expect(resp.msg).eq('success')
+      expect(resp.messages).not.empty
+    })
+    test('throw error when no bot id', async () => {
+      try {
+        await q.chat({
+          botID: '',
+          user: 'yiijie',
+          query: '你好',
+          stream: false
+        })
+      } catch (e) {
+        expect(e).instanceOf(Error)
+        expect(e).property('message').eq('[702242001] 参数传递错误，请查阅对应 API 文档')
+      }
+    })
+  })
+  describe('stream', () => {
+    test('base', async () => {
+      const resp = await q.chat({
+        botID,
+        user: 'yiijie',
+        query: '你好',
+        stream: true
+      })
+      expectTypeOf(resp).toEqualTypeOf<AsyncGenerator<CozeQuesterChatRespIterItem>>()
+      let count = 0
+      const doneCalled = vi.fn()
+      for await (const item of resp) {
+        expectTypeOf(item).toEqualTypeOf<CozeQuesterChatRespIterItem>()
+        if (item.event === 'done') {
+          doneCalled()
+          return
+        }
+        if (item.event === 'error') {
+          throw new Error(item.errorInformation.msg)
+        }
+        const { seqID } = item
+        expect(seqID).eq(count)
+        count++
+      }
+      expect(count).gt(0)
+      expect(doneCalled).toBeCalled()
+    })
+    test('throw error when no bot id', async () => {
+      const resp = await q.chat({
+        botID: '',
+        user: 'yiijie',
+        query: '你好',
+        stream: true
+      })
+      for await (const item of resp) {
+        expectTypeOf(item).toEqualTypeOf<CozeQuesterChatRespIterItem>()
+        if (item.event === 'error') {
+          expect(item.errorInformation.code).eq(702242001)
+          expect(item.errorInformation.msg).eq('参数传递错误，请查阅对应 API 文档')
+          return
+        } else {
+          throw new Error('should throw error')
+        }
+      }
+    })
+  })
+})
