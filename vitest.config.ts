dfed7679132cfc282507b1977f7d61b9efc93a55
yijie4188 <yijie4188@gmail.com>
2024-06-26 02:34:08 +0800

    chore: add test dotenv

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,14 @@
 /// <reference types="vitest" />
+import { config } from 'dotenv'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  test: {}
+  test: {
+    setupFiles: ['dotenv/config'],
+    env: {
+      ...config().parsed,
+      ...config({ path: '.test.env' }).parsed,
+      ...config({ path: '.test.local.env' }).parsed
+    }
+  }
 })
