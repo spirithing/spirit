c9a10f564de8a430b568aed9ed619de8f00d48ac
YiJie <yijie4188@gmail.com>
2025-04-14 02:34:18 +0800

    feat: sort applications

--- renderer/extensions/applications/selectionsGroups.ts
+++ renderer/extensions/applications/selectionsGroups.ts
@@ -1,10 +1,13 @@
 import { useMemo } from 'react'
+import { useTranslation } from 'react-i18next'
 
 import { createUseSelectionsGroups } from '#renderer/.core/define.ts'
 import type { SelectionsGroup } from '#renderer/atoms/sender.ts'
 import { useElectronStore } from '#renderer/hooks/useStore.ts'
 
 export const useSelectionsGroupsForApp = createUseSelectionsGroups(keyword => {
+  const { t } = useTranslation()
+
   const [applications] = useElectronStore('applications')
   return useMemo<SelectionsGroup[]>(() => {
     if (keyword === '') return []
@@ -15,17 +18,25 @@ export const useSelectionsGroupsForApp = createUseSelectionsGroups(keyword => {
     return [
       {
         title: 'result',
-        selections: filtered.map(app => ({
-          icon: app.icon
-            ? { type: 'image', path: app.icon }
-            : { type: 'icon', value: '🚀' },
-          title: app.name.replace(/\.app$/, ''),
-          enterAction: ['open', app.path],
-          operations: [
-            { type: 'text', value: 'Application' }
-          ]
-        }))
+        selections: filtered
+          .sort((a, b) => a.name.localeCompare(b.name))
+          .sort((a, b) => {
+            // 有图标的排前面
+            if (a.icon && !b.icon) return -1
+            if (!a.icon && b.icon) return 1
+            return 0
+          })
+          .map(app => ({
+            title: app.name,
+            icon: app.icon
+              ? { type: 'image', path: app.icon }
+              : { type: 'icon', value: '🚀' },
+            enterAction: ['open', app.path],
+            operations: [
+              { type: 'text', value: t('application.self') }
+            ]
+          }))
       }
     ]
-  }, [applications, keyword])
+  }, [t, applications, keyword])
 })
