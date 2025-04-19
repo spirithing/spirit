a0203587ae2e83f7b41fbdb98972d4e46ff21973
YiJie <yijie4188@gmail.com>
2025-04-20 04:57:05 +0800

    refactor: reuse util function

--- sharing/constants.ts
+++ sharing/constants.ts
@@ -1,3 +1,3 @@
 export const MD_CODE_BLOCK = (lang: string, str: string) => `\`\`\`${lang}\n${str}\n\`\`\``
 
-export const JSON_MD_WRAPPER = (str: string) => `\`\`\`json\n${str}\n\`\`\``
+export const JSON_MD_WRAPPER = (str: string) => MD_CODE_BLOCK('json', str)
